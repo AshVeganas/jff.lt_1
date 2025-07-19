a311ee9fc83a278bfae6a0a0b2a3e0cbf541c5a3
AshVeganas <126359638+AshVeganas@users.noreply.github.com>
2025-07-19 15:23:04 +0300

    Create videos.js

---
+++ js/videos.js
@@ -0,0 +1,11 @@
+// Just paste new YouTube video IDs here
+const videoLinks = [
+  "dQw4w9WgXcQ", // classic
+  "L_jWHffIx5E",
+  "2ZIpFytCSVc",
+  "tVj0ZTS4WF4",
+  "ub82Xb1C8os",
+  "vTIIMJ9tUc8",
+  "XqZsoesa55w",
+  "wWWB9qZgx3E"
+];
