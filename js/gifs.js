81a447394a0b6adfa451f3726d8e42b6b7549704
AshVeganas <126359638+AshVeganas@users.noreply.github.com>
2025-07-19 15:23:24 +0300

    Create gifs.js

---
+++ js/gifs.js
@@ -0,0 +1,11 @@
+// List of cursed GIF URLs
+const gifs = [
+  "https://media.tenor.com/DniD0D6B0N0AAAAi/silly-cat.gif",
+  "https://media.tenor.com/B5uGDKFiYHEAAAAi/spin.gif",
+  "https://media.tenor.com/lzZBYRh0O6YAAAAi/cat-dance.gif",
+  "https://media.tenor.com/TXKHwUu2reIAAAAi/skull-trippy.gif",
+  "https://media.tenor.com/3kEQy0AYCc0AAAAi/pet-the-cat.gif",
+  "https://media.tenor.com/Ug6b1stJyrgAAAAi/wojak-dead-inside.gif",
+  "https://media.tenor.com/xZC5sDClZuUAAAAi/giga-chad-walk.gif",
+  "https://media.tenor.com/I-8RA2tINvIAAAAi/toilet-flush.gif"
+];
